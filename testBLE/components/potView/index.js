f1fd03ff3f7136aa7677c99465e5b34653b39662
Kristian D. Dimitrov <kristiand.dimitrov@telerik.com>
2017-01-25 22:44:05 +0000

    Final extraction of bluetooth logic to bluetooth-service

--- testBLE/components/potView/index.js
+++ testBLE/components/potView/index.js
@@ -1,26 +1,12 @@
 'use strict';
 
-// ASCII only
-function stringToBytes(string) {
-   var array = new Uint8Array(string.length);
-   for (var i = 0, l = string.length; i < l; i++) {
-       array[i] = string.charCodeAt(i);
-    }
-    return array.buffer;
-}
-
-// ASCII only
-function bytesToString(buffer) {
-    return String.fromCharCode.apply(null, new Uint8Array(buffer));
-} 
-
-app.pot = kendo.observable({
+var PotViewModel = kendo.data.ObservableObject.extend({
 	dataBuffer: "",
-	chartSource: new kendo.data.DataSource(),
+	chartSource: null,
 	_consts:{
 		turnOn: "turnOn",
-		setWaterAmmount: "setting:setWaterAmmount",
-		setSoilHumidity: "setting:setSoilHumidity",
+		setWaterAmmount: "set_setting:setWaterAmmount",
+		setSoilHumidity: "set_setting:setSoilHumidity",
 		getData: "getData",
 		getSettings: "getSettings"
 	},
@@ -29,97 +15,55 @@ app.pot = kendo.observable({
 	soilHumidity: "20", 
 	onShow: function (e) {
 		app.pot.id = e.view.params.id;
-		ble.startNotification(app.pot.id, "ffe0", "ffe1", $.proxy(app.pot.onData, app.pot), function(){});
-		window.setTimeout(app.pot.sendMessage.bind(app.pot, [app.pot._consts.getSettings]), 100);
+		app.bluetoothService.startNotification();
+		window.setTimeout(app.bluetoothService.sendMessage.bind(app.bluetoothService, app.pot._consts.getSettings), 100);
 	},
 
 	init: function(){
+		var that = this;
+
+		kendo.data.ObservableObject.fn.init.apply(that, arguments);
+		that.chartSource = new kendo.data.DataSource();
+		app.bluetoothService.bind("data", that.onData.bind(that));
+	},
+	
+
+	disconnect: function(){
+		app.bluetoothService.disconnectCurrent();
 	},
 
-	onData: function(buffer) {
-		// Decode the ArrayBuffer into a typed Array based on the data you expect
-		var that = this,
-			data = bytesToString(buffer),
-			endOfMessageIndex = data.indexOf("!"),
-			indexEnd = data.length - 1;
+	onData: function(data){
+		var that = this;
 
-		if(endOfMessageIndex >= 0){
-			that.dataBuffer = that.dataBuffer + data.substring(0, endOfMessageIndex);
-			if(that.dataBuffer.startsWith("data@")){
-				that.parseData(that.dataBuffer);
-			} else if(that.dataBuffer.startsWith("settings@")){
-				that.parseSettings(that.dataBuffer);
-			}
-
-			that.dataBuffer = data.substring(endOfMessageIndex, indexEnd);
-		} else {
-			that.dataBuffer = that.dataBuffer + data;
+		if(data.dataType === app.bluetoothService.consts.dataLog){
+			this.get("chartSource").data(data.dataObjects);
+		} else if(data.dataType === app.bluetoothService.consts.settingsData){
+			that.set("waterAmount", data.waterAmount);
+			that.set("soilHumidity", data.soilHumidity);
 		}
-	},
-
-	parseData: function(data){
-		var entry,
-			dataEntries,
-			dataObjects = [];
-
-		data = data.replace("data@", "");
-		data = data.replace("!", "");
-		dataEntries = data.split(",");
-
-		for(var i = 0, len = dataEntries.length; i < len; i++ ){
-			entry = dataEntries[i].split("#");
-			dataObjects[i] = {
-				soilHumidity: entry[0],
-				time: new Date(entry[1])
-			}
-		}
-		
-		this.get("chartSource").data(dataObjects);
-	},
-
-	parseSettings: function(data){
-		var that = this,
-			settings = [];
-
-		data = data.replace("settings@", "");
-		settings = data.split("#");
-		that.set("waterAmount", parseInt(settings[0]));
-		that.set("soilHumidity", parseInt(settings[1]));
 	},
 
 	afterShow: function () {},
 	onTurnOn: function () {
 		var that = this;
 		
-		that.sendMessage(that._consts.turnOn);
+		app.bluetoothService.sendMessage(that._consts.turnOn);
 	},
 	onGetData: function(){
 		var that = this;
 		
-		that.sendMessage(that._consts.getData);
+		app.bluetoothService.sendMessage(that._consts.getData);
 	},
 	onWaterChange: function(e){
 		var that = this;
 		
-		that.sendMessage(that._consts.setWaterAmmount + "=" + that.get("waterAmount"));
+		app.bluetoothService.sendMessage(that._consts.setWaterAmmount + "=" + that.get("waterAmount"));
 	},
 	onSoilChange: function(e){
 		var that = this;
 		
-		that.sendMessage(that._consts.setSoilHumidity + "=" + that.get("soilHumidity"));
-	},
-
-	sendMessage: function(message){
-			message = message + "!";
-			ble.write(this.id, "ffe0", "ffe1", stringToBytes(message), function(){
-				alert("send ok");
-			}, function(){
-				alert("send fail");
-			});
+		app.bluetoothService.sendMessage(that._consts.setSoilHumidity + "=" + that.get("soilHumidity"));
 	}
 });
 
-// START_CUSTOM_CODE_home
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_home
+app.pot = new PotViewModel();
--- testBLE/services/bluetooth-service.js
+++ testBLE/services/bluetooth-service.js
@@ -1,5 +1,26 @@
+
+// ASCII only
+function stringToBytes(string) {
+   var array = new Uint8Array(string.length);
+   for (var i = 0, l = string.length; i < l; i++) {
+       array[i] = string.charCodeAt(i);
+    }
+    return array.buffer;
+}
+
+// ASCII only
+function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+} 
+
 var BluetoothService = kendo.Observable.extend({
 	_currentDevice: null,
+	_dataCallback: null,
+	_dataBuffer: "",
+	consts: {
+		dataLog: "dataLog",
+		settingsData: "settingsData"
+	},
 	scan: function(foundCallback) {
 		ble.scan(["ffe0"], 10, function(bleDevice){
 			foundCallback(bleDevice);
@@ -23,9 +44,82 @@ var BluetoothService = kendo.Observable.extend({
 		return def.promise();
 	},
 
+	startNotification: function(dataCallback){
+		var that = this;
+
+		ble.startNotification(that._currentDevice, "ffe0", "ffe1", $.proxy(that.onData, that), function(){});
+	},
+
+	onData: function(buffer) {
+		// Decode the ArrayBuffer into a typed Array based on the data you expect
+		var that = this,
+			data = bytesToString(buffer),
+			endOfMessageIndex = data.indexOf("!"),
+			indexEnd = data.length - 1;
+
+		if(endOfMessageIndex >= 0){
+			that._dataBuffer = that._dataBuffer + data.substring(0, endOfMessageIndex);
+			if(that._dataBuffer.startsWith("data@")){
+				that.parseData(that._dataBuffer);
+			} else if(that._dataBuffer.startsWith("settings@")){
+				that.parseSettings(that._dataBuffer);
+			}
+
+			that._dataBuffer = data.substring(endOfMessageIndex, indexEnd);
+		} else {
+			that._dataBuffer = that._dataBuffer + data;
+		}
+	},
+
+	parseData: function(data){
+		var that = this,
+			entry,
+			dataEntries,
+			dataObjects = [];
+
+		data = data.replace("data@", "");
+		data = data.replace("!", "");
+		dataEntries = data.split(",");
+
+		for(var i = 0, len = dataEntries.length; i < len; i++ ){
+			entry = dataEntries[i].split("#");
+			dataObjects[i] = {
+				soilHumidity: ((1023 - entry[0])/1023)*100,
+				time: new Date(entry[1])
+			}
+		}
+		
+		that.trigger("data", {dataObjects: dataObjects, dataType: that.consts.dataLog});
+	},
+
+	parseSettings: function(data){
+		var that = this,
+			soilHumidity,
+			waterAmount,
+			settings = [];
+
+		data = data.replace("settings@", "");
+		settings = data.split("#");
+		waterAmount = parseInt(settings[0]);
+		soilHumidity = parseInt(settings[1]);
+
+		that.trigger("data", {waterAmount: waterAmount, soilHumidity: soilHumidity, dataType: that.consts.settingsData});
+	},
+
 	disconnectCurrent: function(){
 		if(this._currentDevice){
-			ble.disconnect(this._currentDevice, function(){}, function(){});
+			ble.disconnect(this._currentDevice, function(){
+				app.mobileApp.navigate("components/home/view.html");
+			}, function(){});
 		}
+	},
+
+	sendMessage: function(message){
+			message = message + "!";
+			ble.write(this._currentDevice, "ffe0", "ffe1", stringToBytes(message), function(){
+				alert("send ok");
+			}, function(){
+				alert("send fail");
+			});
 	}
 });
